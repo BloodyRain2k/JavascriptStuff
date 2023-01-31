229ebbc5c4639ff884bedfb7581ad2916109f954
BloodyRain2k <bloodyrain2k@gmx.de>
2023-01-31 11:48:51 +0100

    Update BrowserScriptFunctions.js

--- BrowserScriptFunctions.js
+++ BrowserScriptFunctions.js
@@ -1,14 +1,15 @@
 // #region < functions > //
 
 function addSelectors(elem) { if (!elem) return; elem.xp = (sel) => xp(sel, elem); elem.qsa = (sel) => qsa(sel, elem); elem.qs = (sel) => qs(sel, elem); return elem; };
-function xp(find, root) { let result = []; let elems = document.evaluate(find.replace(/\{([\w-_]+)=([^}]+)\}/, `contains(concat(' ',normalize-space(@$1),' '),' $2 ')`), root || document.body || document, null, XPathResult.ANY_TYPE, null);
+function xp(find, root) { let result = [], elems = document.evaluate(find.replace(/\{([\w-_]+)=([^}]+)\}/, `contains(concat(' ',normalize-space(@$1),' '),' $2 ')`), root || document.body || document, null, XPathResult.ANY_TYPE, null);
     while (!elems.invalidIteratorState) { let elem = elems.iterateNext(); if (elem == null) { break; } result.push(addSelectors(elem)); } return result; }
 function qsa(selector, root) { return Array.from((root || document.body || document).querySelectorAll(selector)).map(elm => addSelectors(elm)); }
 function qs(selector, root) { return addSelectors((root || document.body || document).querySelector(selector)); }
-function waitForElem(selector, root, timeout = 30000) { root ??= document.body || document; let observer, timeoutId = -1; const promise = new Promise((resolve, reject) => {
-    let elem = qs(selector, root); if (elem) { return resolve(elem); } observer = new MutationObserver(() => { let obsElem = qs(selector, root); if (obsElem) { window.clearTimeout(timeoutId);
-    observer.disconnect(); resolve(obsElem); }; }); observer.observe(root, { childList: true, subtree: true }); timeoutId = window.setTimeout(() => { observer.disconnect();
-    reject({ selector, root, timeout }); }, timeout); }); if (observer) promise.observer = observer; if (timeout > 0) promise.timeoutId = timeoutId; promise.maxDelay = timeout; return promise; }
+function waitForElem(selector, root, timeout = 30000) { if (typeof(root) == "number") { timeout = root; root = null; }; root ??= document.body || document; let observer, timeoutId = -1;
+    const promise = new Promise((resolve, reject) => { let elem = qs(selector, root); if (elem) { return resolve(elem); }; observer = new MutationObserver(() => {
+    let obsElem = qs(selector, root); if (obsElem) { window.clearTimeout(timeoutId); observer.disconnect(); resolve(obsElem); }; });
+    observer.observe(root, { childList: true, subtree: true }); timeoutId = window.setTimeout(() => { observer.disconnect(); reject({ selector, root, timeout }); }, timeout); });
+    if (observer) { promise.observer = observer; }; if (timeout > 0) { promise.timeoutId = timeoutId; }; promise.maxDelay = timeout; return promise; }
 
 function getLocalObject(key) { var str = localStorage[key]; return str ? function() { try { return JSON.parse(str); } catch (e) { return undefined; } }() : undefined; }
 function setLocalObject(key, value) { localStorage[key] = JSON.stringify(value); }
@@ -30,3 +31,9 @@ function openNewTab(url){ if (!url.startsWith("http")) { url = "https://" + url;
     evt.initMouseEvent("click", true, true, this, 0, 0, 0, 0, 0, true, false, false, false, 0, null); document.body.appendChild(a); a.dispatchEvent(evt); document.body.removeChild(a); }
 
 // #endregion //
+
+let observer = new MutationObserver(mutation);
+function watch(target, options) { observer.observe(target, options); console.log("watch added:", target, options); }
+function mutation(mutations, observer) {
+    console.log(getCurrentID(), mutations, observer);
+}
