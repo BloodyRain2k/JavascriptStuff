23fe63a6e07a0b018cb14546f5a31d38b81802d6
BloodyRain2k <bloodyrain2k@gmx.de>
2023-05-23 11:41:45 +0200

    forgot version number increase

--- WordPress_improvements.userscript.js
+++ WordPress_improvements.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WordPress improvements
 // @namespace    http://tampermonkey.net/
-// @version      0.3.0.1
+// @version      0.3.0.2
 // @description  try to take over the world!
 // @author       BK
 // @match        */wp-login.php*
