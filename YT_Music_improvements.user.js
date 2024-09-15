a375b0692e423b9cd6e7c7a7cd68ce08f115d8b4
Bloody <bloody@EnermaxDebian>
2024-09-15 19:48:18 +0200

    forgot to increase version with recent changes

--- YT_Music_improvements.user.js
+++ YT_Music_improvements.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YT Music improvements
-// @version      0.3.7.19
+// @version      0.3.7.20
 // @namespace    http://tampermonkey.net/
 // @description
 // @author       BloodyRain2k
